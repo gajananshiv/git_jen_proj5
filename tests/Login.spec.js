d04208b716757b289a3ea3442e79da3b2e4c3917
Gajashiv10 <gajashiv2728@gmail.com>
2026-08-18 14:01:03 +0530

    added new line

--- tests/Login.spec.js
+++ tests/Login.spec.js
@@ -4,7 +4,10 @@ import {test} from '@playwright/test'
 test("TC NAME",async({page}) =>{
 
     await page.goto("https://www.google.com/")
+    await page.locator("//a[@aria-label='Gmail ']").click()
+    
     await page.waitForTimeout(3000)
+
 
 
 })
