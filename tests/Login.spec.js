ffd09140ac16a5b35a57c48f9e54fa6946c93cf4
Gajashiv10 <gajashiv2728@gmail.com>
2026-08-18 14:13:17 +0530

    removed

--- tests/Login.spec.js
+++ tests/Login.spec.js
@@ -5,7 +5,7 @@ test("TC NAME",async({page}) =>{
 
     await page.goto("https://www.google.com/")
     await page.locator("//a[@aria-label='Gmail ']").click()
-    console.log("line number8")
+    
     
     await page.waitForTimeout(3000)
 
